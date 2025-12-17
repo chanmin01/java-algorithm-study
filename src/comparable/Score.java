17f268a61da500ab4269b4a2d98b7af656fbdaa4
jeonchanmin <chanmin010702@gmail.com>
2025-12-17 15:54:56 +0900

    :sparkles: feat: comparable을 통한 다중 조건 정렬

---
+++ src/comparable/Score.java
@@ -0,0 +1,22 @@
+package comparable;
+
+public class Score implements Comparable<Score>{
+    int english;
+    int math;
+
+    public Score(int english, int math) {
+        this.english = english;
+        this.math = math;
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" + "english=" + english + ", math=" + math + "}";
+    }
+
+    @Override
+    public int compareTo(Score o) {
+        if (this.english == o.english) return o.math - this.math;
+        return o.english - this.english;
+    }
+}
