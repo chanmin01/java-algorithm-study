2c4e79a93f2273d056598c6f5619363dc366255f
jeonchanmin <chanmin010702@gmail.com>
2025-12-18 20:20:54 +0900

    :sparkles: feat: 백준 11720번 풀이

---
+++ src/chapter04/arrayandlist/boj_11720/Main.java
@@ -0,0 +1,23 @@
+package chapter04.arrayandlist.boj_11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String sNum = br.readLine();
+        int sum = 0;
+
+        char[] cNum = sNum.toCharArray();
+
+        for (char num : cNum) {
+            sum += num - '0';
+        }
+
+        System.out.println(sum);
+
+    }
+}
