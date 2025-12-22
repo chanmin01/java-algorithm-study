a908bf92b2ca62d8c109d0115e5bbfdc99b1946c
jeonchanmin <chanmin010702@gmail.com>
2025-12-22 17:35:21 +0900

    :sparkles: feat: 백준 2018번 풀이

---
+++ src/chapter04/boj_2018/Main.java
@@ -0,0 +1,33 @@
+package chapter04.boj_2018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+        while (end_index != N) {
+            if (sum == N) {
+                end_index++;
+                count++;
+                sum += end_index;
+            } else if (sum > N) {
+                sum -= start_index;
+                start_index++;
+            } else if (sum < N) {
+                end_index++;
+                sum += end_index;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
