7d2f08b830a17b8fb5e8dc3c16808359ada48889
jeonchanmin <chanmin010702@gmail.com>
2026-01-07 13:43:59 +0900

    :sparkles: feat: 백준 1427번 풀이

---
+++ src/chapter05/boj_1427/Main.java
@@ -0,0 +1,39 @@
+package chapter05.boj_1427;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] A = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            A[i] = str.charAt(i) - '0';
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            int Max = i;
+            for (int j = i + 1; j < str.length(); j++) {
+                if (A[j] > A[Max]) {
+                    Max = j;
+                }
+            }
+            if (A[i] < A[Max]) {
+                int temp = A[i];
+                A[i] = A[Max];
+                A[Max] = temp;
+            }
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(A[i]);
+        }
+
+
+
+
+
+    }
+}
