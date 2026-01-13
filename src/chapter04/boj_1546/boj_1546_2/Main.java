9dfccbec0f8da17c9172b14bd6d37f11278832a7
jeonchanmin <chanmin010702@gmail.com>
2026-01-13 17:12:41 +0900

    :sparkles: feat: 백준 1546번 2번째 풀이

---
+++ src/chapter04/boj_1546/boj_1546_2/Main.java
@@ -0,0 +1,32 @@
+package chapter04.boj_1546.boj_1546_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        int Max = A[N - 1];
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += A[i];
+        }
+
+        double result = (double) sum / Max * 100 / N;
+
+        System.out.println(result);
+
+    }
+}
