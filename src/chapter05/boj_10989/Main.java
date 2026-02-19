0d83d4e2bb755a6c0f53ced8ac51a059c62c1fff
jeonchanmin <chanmin010702@gmail.com>
2026-02-19 17:07:46 +0900

    :sparkles: feat: 백준 10989번 풀이

--- src/chapter05/boj_10989/Main.java
+++ src/chapter05/boj_10989/Main.java
@@ -7,13 +7,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int N = Integer.parseInt(br.readLine());
         A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
-        br.close();
         Radix_Sort(A, 5);
+
         for (int i = 0; i < N; i++) {
             bw.write(A[i] + "\n");
         }
@@ -21,12 +22,14 @@ public class Main {
         bw.close();
     }
 
-    private static void Radix_Sort(int[] A, int max_size) {
+    public static void Radix_Sort(int[] A, int max_size) {
         int[] output = new int[A.length];
-        int jarisu = 1;
         int count = 0;
+        int jarisu = 1;
+
         while (count != max_size) {
             int[] bucket = new int[10];
+
             for (int i = 0; i < A.length; i++) {
                 bucket[(A[i] / jarisu) % 10]++;
             }
@@ -35,16 +38,17 @@ public class Main {
             }
 
             for (int i = A.length - 1; i >= 0; i--) {
-                output[bucket[(A[i] / jarisu % 10)] - 1] = A[i];
+                output[bucket[(A[i] / jarisu) % 10] - 1] = A[i];
                 bucket[(A[i] / jarisu) % 10]--;
             }
 
             for (int i = 0; i < A.length; i++) {
                 A[i] = output[i];
             }
+
             jarisu = jarisu * 10;
             count++;
         }
-    }
 
+    }
 }
