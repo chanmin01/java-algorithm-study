f857c6481a6c8558a415c38422add7be62c1b4a2
jeonchanmin <chanmin010702@gmail.com>
2026-03-01 17:37:53 +0900

    :sparkles: feat: 백준 17136번 풀이

--- src/chapter06/boj_17136/Main.java
+++ src/chapter06/boj_17136/Main.java
@@ -7,13 +7,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int[][] M = new int[10][10];
     static int[] S = {0, 5, 5, 5, 5, 5};
     static int result = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         for (int i = 0; i < 10; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 10; j++) {
@@ -28,10 +28,9 @@ public class Main {
         } else {
             System.out.println(result);
         }
-
     }
 
-    public static void backtracking(int xy, int useCnt) {
+    private static void backtracking(int xy, int useCnt) {
         if (xy == 100) {
             result = Math.min(result, useCnt);
             return;
@@ -40,14 +39,15 @@ public class Main {
         int y = xy / 10;
 
         if (result <= useCnt) return;
+
         if (M[y][x] == 1) {
             for (int i = 5; i > 0; i--) {
                 if (S[i] > 0 && check(x, y, i)) {
-                    S[i]--;
                     fill(x, y, i, 0);
+                    S[i]--;
                     backtracking(xy + 1, useCnt + 1);
-                    S[i]++;
                     fill(x, y, i, 1);
+                    S[i]++;
                 }
             }
         } else {
@@ -55,18 +55,11 @@ public class Main {
         }
     }
 
-    private static void fill(int x, int y, int size, int num) {
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
-                M[i][j] = num;
-            }
-        }
-    }
-
-    public static boolean check(int x, int y, int size) {
+    private static boolean check(int x, int y, int size) {
         if (x + size > 10 || y + size > 10) {
             return false;
         }
+
         for (int i = y; i < y + size; i++) {
             for (int j = x; j < x + size; j++) {
                 if (M[i][j] != 1) {
@@ -74,8 +67,17 @@ public class Main {
                 }
             }
         }
+
         return true;
     }
 
-}
+        private static void fill ( int x, int y, int size, int num){
+            for (int i = y; i < y + size; i++) {
+                for (int j = x; j < x + size; j++) {
+                    M[i][j] = num;
+                }
+            }
+        }
+
+    }
 
