54e62e6a13a300660a8838715a7abeb073657234
jeonchanmin <chanmin010702@gmail.com>
2026-03-25 14:55:38 +0900

    :sparkles: feat: 백준 1016번 풀이

--- src/chapter08/boj_1016/Main.java
+++ src/chapter08/boj_1016/Main.java
@@ -11,24 +11,23 @@ public class Main {
 
         boolean[] Check = new boolean[(int) (Max - Min + 1)];
 
-        for (long i = 2; i * i <= Max; i++) {
+        for (long i = 2; i <= Math.sqrt(Max); i++) {
             long pow = i * i;
             long start_index = Min / pow;
             if (Min % pow != 0) {
                 start_index++;
             }
             for (long j = start_index; pow * j <= Max; j++) {
-                Check[(int)((j * pow) - Min)] = true;
+                Check[(int) ((j * pow) - Min)] = true;
             }
-        }
 
+        }
         int count = 0;
         for (int i = 0; i <= Max - Min; i++) {
             if (!Check[i]) {
                 count++;
             }
         }
-
         System.out.println(count);
     }
 }
