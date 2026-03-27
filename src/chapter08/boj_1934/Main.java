724fd6d9e37d5077632eee9600472f9dce3cf1f5
jeonchanmin <chanmin010702@gmail.com>
2026-03-27 12:58:50 +0900

    :sparkles: feat: 백준 1934번 풀이

--- src/chapter08/boj_1934/Main.java
+++ src/chapter08/boj_1934/Main.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int result = a * b / gcd(a, b);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            int result = 0;
+            result = A * B / gcd(A, B);
             System.out.println(result);
         }
     }
