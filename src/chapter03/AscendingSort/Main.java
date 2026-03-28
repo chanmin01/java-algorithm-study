f6ddf4c08dac18bf67e69eb0f71930dd90d29c3d
jeonchanmin <chanmin010702@gmail.com>
2026-03-28 19:46:01 +0900

    :sparkles: feat: AscendingSort 구현

---
+++ src/chapter03/AscendingSort/Main.java
@@ -0,0 +1,11 @@
+package chapter03.AscendingSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] A = {5, 3, 2, 4, 1};
+        Arrays.sort(A);
+        System.out.println(Arrays.toString(A));
+    }
+}
