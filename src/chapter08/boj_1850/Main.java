a9f039a07a9ee822dfb815daff493882ee40038e
jeonchanmin <chanmin010702@gmail.com>
2026-03-28 19:31:25 +0900

    :sparkles: feat: 백준 1850번 풀이

---
+++ src/chapter08/boj_1850/Main.java
@@ -0,0 +1,34 @@
+package chapter08.boj_1850;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+        long result = gcd(a, b);
+        while (result > 0) {
+            bw.write("1");
+            result--;
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+}
+
