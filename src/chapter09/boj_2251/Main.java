4df3cbe7f54e0b3890de916e5b1606ef45bac029
jeonchanmin <chanmin010702@gmail.com>
2026-05-06 17:55:57 +0900

    :sparkles: feat: 백준 2251번 풀이

--- src/chapter09/boj_2251/Main.java
+++ src/chapter09/boj_2251/Main.java
@@ -1,47 +1,57 @@
 package chapter09.boj_2251;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[] Sender = {0, 0, 1, 1, 2, 2};
     static int[] Receiver = {1, 2, 0, 2, 0, 1};
-    static boolean visited[][];
-    static boolean answer[];
-    static int now[];
+    static boolean[][] visited;
+    static boolean[] answer;
+    static int[] capacity;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        now = new int[3];
-        now[0] = scan.nextInt();
-        now[1] = scan.nextInt();
-        now[2] = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        capacity = new int[3];
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        capacity[0] = A;
+        capacity[1] = B;
+        capacity[2] = C;
         visited = new boolean[201][201];
         answer = new boolean[201];
         BFS();
+
         for (int i = 0; i < answer.length; i++) {
-            if (answer[i]) System.out.print(i + " ");
+            if (answer[i]) {
+                System.out.print(i + " ");
+            }
         }
     }
-
-    private static void BFS() {
+    public static void BFS() {
         Queue<AB> queue = new LinkedList<>();
         queue.add(new AB(0, 0));
         visited[0][0] = true;
-        answer[now[2]] = true;
+        answer[capacity[2]] = true;
         while (!queue.isEmpty()) {
             AB p = queue.poll();
             int A = p.A;
             int B = p.B;
-            int C = now[2] - A - B;
-            for (int k = 0; k < 6; k++) {
+            int C = capacity[2] - A - B;
+            int[] now = {A, B, C};
+            for (int i = 0; i < 6; i++) {
                 int[] next = {A, B, C};
-                next[Receiver[k]] += next[Sender[k]];
-                next[Sender[k]] = 0;
-                if (next[Receiver[k]] > now[Receiver[k]]) {
-                    next[Sender[k]] = next[Receiver[k]] - now[Receiver[k]];
-                    next[Receiver[k]] = now[Receiver[k]];
+                next[Receiver[i]] += now[Sender[i]];
+                next[Sender[i]] = 0;
+                if (next[Receiver[i]] > capacity[Receiver[i]]) {
+                    next[Sender[i]] = next[Receiver[i]] - capacity[Receiver[i]];
+                    next[Receiver[i]] = capacity[Receiver[i]];
                 }
                 if (!visited[next[0]][next[1]]) {
                     visited[next[0]][next[1]] = true;
@@ -52,6 +62,7 @@ public class Main {
                 }
             }
         }
+
     }
 }
 
@@ -64,4 +75,5 @@ class AB {
         this.B = B;
     }
 }
+
 
