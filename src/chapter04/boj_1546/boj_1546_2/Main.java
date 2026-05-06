67bb0daf5d2ded37caebdd95d97d9d5504a72979
jeonchanmin <chanmin010702@gmail.com>
2026-05-06 16:42:42 +0900

    :sparkles: feat: 백준 1546번 복습

--- src/chapter04/boj_1546/boj_1546_2/Main.java
+++ src/chapter04/boj_1546/boj_1546_2/Main.java
@@ -11,15 +11,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] scores = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long Mx = Arrays.stream(num).max().getAsInt();
         long sum = 0;
         for (int i = 0; i < N; i++) {
-            scores[i] = Integer.parseInt(st.nextToken());
-            sum += scores[i];
+            sum += num[i];
         }
-        long maxVal = Arrays.stream(scores).max().getAsInt();
-        double result = (double)sum * 100 / maxVal / N;
+
+        double result = (double) sum / Mx * 100 / N;
         System.out.println(result);
 
     }
